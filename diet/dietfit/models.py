3c2fee7f5094397ef6e5127200a69927eb8435fa
artchar <arthr2@ucla.edu>
2015-05-06 08:16:40 -0700

    extending user model

--- diet/dietfit/models.py
+++ diet/dietfit/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# class Food(models.Model):
+
+class UserProfile(models.Model):
+	GENDER_CHOICES = (
+		('M', 'Male'),
+		('F', 'Female')
+		)
+	user = models.OneToOneField(User)
+	gender = models.CharField(max_length=1)
